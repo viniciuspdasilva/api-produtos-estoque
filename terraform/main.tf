24953360f68d29de14f9e7c6aa110f18e6a29f7f
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2026-08-20 20:52:27 -0300

    Add AWS EC2 key pair resource, SSH public key variable, instance bootstrapping script, and Dockerfile with Quarkus runtime configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 # terraform/main.tf
 
+variable "ssh_public_key" {
+  description = "Chave publica SSH para acessar a instancia"
+  type = string
+  
+}
+
 # 1.Aws provider
 terraform {
   required_providers {
@@ -14,17 +20,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "deployer_key" {
+  key_name = "github-actions-key"
+  public_key = var.ssh_public_key
+}
+
 # 2.Firewall configuration
 
 resource "aws_security_group" "api_sg" {
-  name = "api-produtos-sg"
+  name        = "api-produtos-sg"
   description = "Regras de rede para a API de Produtos"
 
   ingress {
     description = "SSH Access"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -54,9 +65,34 @@ resource "aws_instance" "api_server" {
   # Associa o firewall criado acima a este servidor
   vpc_security_group_ids = [aws_security_group.api_sg.id]
 
+  # -------------------------------------------------------------
+  # SCRIPT DE INICIALIZAÇÃO (Bootstrapping)
+  # Roda automaticamente quando a máquina é criada
+  # -------------------------------------------------------------
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # Atualiza os pacotes do Ubuntu
+    apt-get update -y
+
+    # Instala dependências básicas
+    apt-get install -y wget curl unzip
+
+    # Instala a JDK 25 nativa do repositório oficial
+    apt-get install -y openjdk-25-jdk
+
+    # Instala o docker
+    apt-get install -y docker.io
+
+    # Dá permissão para o usuário 'ubuntu' rodar o Docker
+    usermod -aG docker ubuntu
+
+  EOF
+
   tags = {
     Name        = "Servidor-API-Produtos"
     Environment = "Dev"
     Project     = "Fase1-DevOps"
   }
 }
+
